a8ec84fce9661ef5f5c7a3260bdb4497070e7c04
khojiakbarr <chinozbekov09@gmail.com>
2024-09-09 20:55:13 +0500

    seo op

--- app/layout.js
+++ app/layout.js
@@ -11,17 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <Head>
-        <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width"
-          key="viewport"
-        />
-        <meta
-          name="google-site-verification"
-          content="tAAmBHlEVjOFK6kjcOizJO_CrkGepXjtudO2_mI5Hs0"
-        />
-      </Head>
       <link rel="icon" href="/Kontur_logo.svg" sizes="any" />
       <body className={inter.className}>{children}</body>
     </html>
--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -8,10 +8,10 @@ export default function Hero() {
       <div className="pt-[40px] md:pt-[80px] container mx-auto px-[15px]">
         <div className="wrapper  relative z-[0]  flex justify-between">
           <div className="hero_title">
-            <h6 className="md:text-[24px] font-[400] text-black flex items-center gap-1 mb-[5px]">
+            <p className="md:text-[24px] font-[400] text-black flex items-center gap-1 mb-[5px]">
               <p className=" relative z-[10] w-[22px] h-[22px] bg-green-400 rounded-[30px]"></p>
               Ежедневно с 9:00 до 18:00
-            </h6>
+            </p>
             <h1 className="md:text-[40px] w-[375px] md:w-[700px] text-[#593f0d]">
               Дизайн интерьера, ремонт жилой и коммерческой недвижимости <br />
               <span className="text-[#ffcf6e] font-bold md:font-[400]">
@@ -19,9 +19,9 @@ export default function Hero() {
               </span>
             </h1>
             <p className=" h-[3px] w-[40px] md:w-[100px] bg-[#ffcf6e] my-[7px] md:my-[10px]"></p>
-            <h3 className=" md:text-[24px] font-[400] w-[250px] md:w-[400px]">
+            <h2 className=" md:text-[24px] font-[400] w-[250px] md:w-[400px]">
               Выполним ремонт точно в срок с гарантией по договору
-            </h3>
+            </h2>
 
             <Link href={"#form_info"}>
               <button className="bg_color px-[10px] relative button md:px-[24px] py-[10px] md:py-[24px] shadow-xl mt-[30px] mb-[30px] md:my-[40px] rounded-[10px] text-[10px] md:text-[14px] font-[700] cursor-pointer">
@@ -52,9 +52,9 @@ export default function Hero() {
               </div>
               <div className="rounded-[20px] h-[306px]">
                 <div className=" w-[320px] backdrop-blur-[13px] flex flex-col items-center justify-center p-[20px] rounded-[20px]  ">
-                  <p className="font-[400] text-[18px] text-center text-white ">
+                  <h2 className="font-[400] text-[18px] text-center text-white ">
                     Специальные условия от поставщиков мебели для наших клиентов
-                  </p>
+                  </h2>
                   <img
                     src={"/qalam_ruchka_icon.png"}
                     alt="qalam"
